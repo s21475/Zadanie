76b7074e9cb2eb8b0d7cc41b9df58ce08a03ef8e
Krzysztof Lewandowski <s20491@pjwstk.edu.pl>
2019-10-24 12:56:40 +0200

    pierwsza silnia

---
+++ PRG1(zad17).cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	int nn=1, kk=1, nk=1, wynik;
+	cout << "Ilu elementowy jest zbior? " << endl;
+	cin >> n;
+	cout << "Ile elementow ma byc w podzbiorach? " << endl;
+	cin >> k;
+	//Zrobiłem pierwszą silnię
+	for(int i =1;i<=n;i++)
+	{
+		nn *= i;
+	}
+	// Zadanie wykonane
+
+	
+}
