10542c30dfc4581e2b160817b44f4d3a66b7b081
Mikolaj Noga <s21475@pjwstk.edu.pl>
2019-10-24 12:39:10 +0200

    wszystko

---
+++ PRG1(zad17)/PRG1(zad17).cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	int nn=1, kk=1, nk=1, wynik;
+	cout << "Ilu elementowy jest zbior? " << endl;
+	cin >> n;
+	cout << "Ile elementow ma byc w podzbiorach? " << endl;
+	cin >> k;
+	for(int i =1;i<=n;i++)
+	{
+		nn *= i;
+	}
+	for(int i=1;i<=k;i++)
+	{
+		kk *= i;
+	}
+	for (int i = 1; i <=(n-k); i++)
+	{
+		nk *= i;
+	}
+	wynik = nn / (kk * nk);
+	cout << wynik;
+
+	
+}
